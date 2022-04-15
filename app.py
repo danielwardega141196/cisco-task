08870ec1235071ef2beb9540d5740e47f0b627f1
Daniel Wardega <danielwardega141196@gmail.com>
2022-04-15 11:00:04 +0200

    changed api name

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify, make_response
+from flask import Flask
 from flask_swagger_ui import get_swaggerui_blueprint
+
 from routes.api import API
 
 APP = Flask(__name__)
@@ -11,7 +12,7 @@ SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
     base_url=SWAGGER_URL,
     api_url=API_URL,
     config={
-        'app_name': "Seans-Python-Flask-REST-Boilerplate"
+        'app_name': "Cisco Coding Task"
     }
 )
 APP.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
